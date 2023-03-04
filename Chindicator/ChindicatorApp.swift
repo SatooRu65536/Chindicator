d59098e7b905fcff7b28d31f0284d41805830bb4
SatooRu65536 <ryuto4619@gmail.com>
2023-03-04 22:51:12 +0900

    Initial Commit

---
+++ Chindicator/ChindicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChindicatorApp.swift
+//  Chindicator
+//
+//  Created by 多田隆人 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct ChindicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
